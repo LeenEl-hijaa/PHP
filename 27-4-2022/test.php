9338f0714f215449c740e7ec529fe797c8a352b1
LeenEl-hijaa <leenkhaled77@gamil.com>
2022-04-28 04:54:43 +0300

    main

---
+++ 27-4-2022/test.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style></style>
+</head>
+<body>
+    <h1>Group 7</h1>
+    <?php 
+//    if(array_key_exists(key,array)){
+//     function
+// } 
+
+    ?>
+    <form method="post">
+        <input type="submit" name="read" value="button1">
+        <input type="submit" name="read2" value="button2">
+
+
+    </form>
+</body>
+</html>
+
+
+ <!-- if(array_key_exists("read",$_POST)){
+     button();
+    }
+    if(array_key_exists("read2",$_POST)){
+        button2();
+       }
+    function button(){
+        echo "Welcome from group 7";
+    }
+    function button2(){
+        echo "Say Hi";
+    } -->
